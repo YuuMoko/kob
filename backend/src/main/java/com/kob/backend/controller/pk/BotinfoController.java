0ee7e1ca4e1eec7d1f3cfe1e650e8a98b66fa369
YuuMoko <2772337221@qq.com>
2024-05-29 14:34:21 +0800

    创建后端

--- backend/src/main/java/com/kob/backend/controller/pk/BotinfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotinfoController.java
@@ -2,20 +2,26 @@ package com.kob.backend.controller.pk;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 @RestController()
 @RequestMapping("/pk/")
 public class BotinfoController {
 
     @RequestMapping("getbotinfo/")
-    public List<String> getBotInfo() {
-        List<String> list = new LinkedList<String>();
-        list.add("sword");
-        list.add("tiger");
-        list.add("apple");
+    public List<Map<String, String>> getBotInfo(){
+        List<Map<String, String>> list = new LinkedList<>();
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "tiger");
+        bot1.put("ratring", "1500");
+
+        Map<String, String> bot2 = new HashMap<>();
+        bot2.put("name", "apple");
+        bot2.put("ratring", "1800");
+
+        list.add(bot1);
+        list.add(bot2);
+
         return list;
     }
 }
