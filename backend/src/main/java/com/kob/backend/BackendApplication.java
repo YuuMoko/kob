95c40c048ba67194a700e5d6d2927f2bbdb658d8
YuuMoko <2772337221@qq.com>
2024-05-29 14:18:44 +0800

    Learn. 5/29

--- backend/src/main/java/com/kob/backend/BackendApplication.java
+++ backend/src/main/java/com/kob/backend/BackendApplication.java
@@ -3,6 +3,8 @@ package com.kob.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class BackendApplication {
 
