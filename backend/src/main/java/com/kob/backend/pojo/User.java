72847735688115ce8ccb7c052364941327be7b0f
YuuMoko <2772337221@qq.com>
2024-06-11 21:15:01 +0800

    4.1 配置MySQL与注册登录模块（一）

---
+++ backend/src/main/java/com/kob/backend/pojo/User.java
@@ -0,0 +1,16 @@
+package com.kob.backend.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+    Integer id;
+    String username;
+    String password;
+}
