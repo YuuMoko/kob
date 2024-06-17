73e0ae7ec3b233d2bb05dcd0beebc58d21d51a63
YuuMoko <2772337221@qq.com>
2024-06-17 12:49:48 +0800

    5.1 创建个人中心页面(上)

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,37 +2,14 @@
   <NavBar></NavBar>
   <router-view></router-view>
 </template>
-
 <script>
 import NavBar from './components/NavBar.vue';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
-import $ from 'jquery'
 export default {
   components: {
     NavBar
   },
-
-  setup() {
-
-
-
-    $.ajax({
-      url: "http://127.0.0.1:3000/user/account/register/",
-      type: "post",
-      data: {
-        username: 'yx231',
-        password: "123",
-        confirmedPassword: "123",
-      },
-      success(resp) {
-        console.log(resp)
-      },
-      error(resp) {
-        console.log(resp)
-      }
-    })
-  }
 }
 </script>
 
